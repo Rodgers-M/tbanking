9eeb9490659246bf378144b6b25d6fcd61f16094
Rodgers-M <mkishagha@gmail.com>
2017-02-13 15:28:32 +0300

    add the user model

---
+++ app/models/user.js
@@ -0,0 +1,30 @@
+var mongoose      =   require('mongoose');
+var bcrypt        =   require('bcrypt-nodejs')
+var crypto        =   require('crypto');
+var Schema        =   mongoose.Schema;
+var Role          =   require('../../app/models/roles');
+
+var userSchema    = new Schema({
+  local: {
+      username  :  { type: String },
+      firstname :  { type: String },
+      lastname  :  { type: String },
+      password  :  { type: String },
+      email     :  { type: String },
+      shares    :  { type: Number },
+      role      :  [{type: Schema.Types.ObjectId, ref:Role }]
+    }
+});
+
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+module.exports = mongoose.model('User', userSchema);
