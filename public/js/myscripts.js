14a53866cb4920ec7a6e25e1076f787a65071ec6
Rodgers-M <mkishagha@gmail.com>
2017-02-13 15:46:08 +0300

    add the js scripts file

---
+++ public/js/myscripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+
+	$('#tbinfo').waypoint(function(direction){
+
+		if(direction=="down"){
+
+			$('nav').addClass('sticky');
+		} else{
+			$('nav').removeClass('sticky');
+		}
+	} , {
+		offset: '90%;'
+	});
+
+});
