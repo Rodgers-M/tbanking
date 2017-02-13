1a19a7e25b4a008d48693f8e84a97bf35900b1c0
Rodgers-M <mkishagha@gmail.com>
2017-02-13 15:32:27 +0300

    add the events model

---
+++ app/models/events.js
@@ -0,0 +1,17 @@
+var mongoose      =   require('mongoose');
+var Schema        =   mongoose.Schema;
+
+var EventSchema   = new Schema({
+    title        : { type : String },
+    venue        : {type  : String },
+    description  : {type  : String },
+    date         : {type  : Date },
+    time         : {type  : String }
+});
+
+EventSchema.methods.getEventByTitle = function(title, callback){
+  var query = {'title': title};
+  Event.findOne(query,callback);
+};
+
+module.exports = mongoose.model('Event', EventSchema);
