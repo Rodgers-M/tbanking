37af3c001b7d4a2579f6fa63ab37019c3d0d3089
Rodgers-M <mkishagha@gmail.com>
2017-02-16 09:00:43 +0300

    group related routes together

--- config/routes/index.js
+++ config/routes/index.js
@@ -1,11 +1,12 @@
-var express        =  require('express');
-var passport       =  require('passport');
-var router         =  express.Router();
-var homeRoutes     =  require('./home');
-var sessionRoutes  = require('./session');
-var userRoutes     = require('./user');
-var eventRoutes    = require('./events');
-var  roleRoutes    = require('./roles');
+var express         =  require('express');
+var passport        =  require('passport');
+var router          =  express.Router();
+var homeRoutes      =  require('./home');
+var sessionRoutes   = require('./session');
+var userRoutes      = require('./user');
+var eventRoutes     = require('./events');
+var  roleRoutes     = require('./roles');
+var  sharesRoutes   = require('./shares');
 
 function isLoggedIn(req, res, next) {
   if(req.isAuthenticated()){
@@ -15,10 +16,11 @@ function isLoggedIn(req, res, next) {
   res.redirect('/login');
 }
 
+//user routes
 router.get('/', homeRoutes.index);
 router.get('/login', sessionRoutes.new);
 router.post('/session/create', sessionRoutes.create
-  
+
 );
 router.get('/logout', sessionRoutes.delete);
 router.get('/signup', userRoutes.new );
@@ -28,18 +30,23 @@ router.post('/user/create',passport.authenticate('local-signup', {
         failureFlash : true // allow flash messages
     }));
 
+//roles routes
 router.get('/roles/:role',  roleRoutes.create);
 router.get('/roles', roleRoutes.index);
 router.post('/role', roleRoutes.assign);
 
-router.get('/shares', function(req, res){
-  res.render('userdashboard/shares');
-});
+//shares routes
+router.get('/manageshares', sharesRoutes.new);
+router.post('/updateshares', sharesRoutes.update);
+router.get('/shares', sharesRoutes.userShares);
 router.get('/loancalc', function(req, res){
-  res.render('userdashboard/loancalc');
+  res.send("your loan will be calculated here");
 });
+
+//events routes
 router.get('/newevent', eventRoutes.new);
 router.post('/events/new', eventRoutes.create);
 router.get('/events', eventRoutes.index);
 router.get('/userevents', eventRoutes.userevent);
+
 module.exports = router;
