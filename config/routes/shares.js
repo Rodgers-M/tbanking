330410002426785976fbf9283919f1c6a247fc02
Rodgers-M <mkishagha@gmail.com>
2017-02-16 09:05:33 +0300

    add redirects and flash messages

--- config/routes/shares.js
+++ config/routes/shares.js
@@ -1,8 +1,18 @@
 var User = require('../../app/models/user');
 
 module.exports ={
-  view : function(req, resave){
-    // define a function to view user's shares
+  new  : function(req, res){
+    res.render('admindashboard/shares',{
+      page :'shares'
+    });
+  },
+  userShares : function(req, res){
+    res.render('userdashboard/shares',{
+      page : 'shares'
+    });
+  },
+  view : function(req, res){
+    // define a function to view total group shares
   },
   update : function(req, res){
     username        = req.body.username;
@@ -10,14 +20,15 @@ module.exports ={
 
     User.findOne({"local.username" : username}, function(err, user){
       if(err) return err;
+      console.log(user);
       currentShares   = user.local.shares;
       newShares       = currentShares + sharesBought;
-
+      console.log(newShares);
       User.update({"local.username" : user.local.username}, {$set:{"local.shares": newShares}},
       function(err, user){
         if(err) return err;
         console.log(user);
-        res.send("shares updated successfully");
+        req.flash('success', "shares updated successfully");
         res.redirect('/manageshares');
       }
     );
