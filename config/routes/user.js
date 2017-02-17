691f912169d47fa75ccab18ba533e2e9bbaa6f0e
Rodgers-M <mkishagha@gmail.com>
2017-02-17 13:33:21 +0300

    remove the message field when rendering the signup view

--- config/routes/user.js
+++ config/routes/user.js
@@ -2,7 +2,6 @@ var User          = require('../../app/models/user');
 module.exports ={
   new : function(req, res){
     res.render('signup', {
-       message : req.flash('signupMessage'),
        title   : "Sign Up"
     });
   }
