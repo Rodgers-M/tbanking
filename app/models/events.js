d5c20c705d646f3d2ee75972101015f47466d4e1
Rodgers-M <mkishagha@gmail.com>
2017-04-17 14:56:35 +0300

    add slug to be used to generate friendly urls for a particular event

--- app/models/events.js
+++ app/models/events.js
@@ -3,6 +3,7 @@ var Schema        =   mongoose.Schema;
 
 var EventSchema   = new Schema({
     title        : { type : String, unique:true },
+	slug		 : {type : String},
     venue        : { type : String },
     description  : { type : String },
     date         : { type : Date },
@@ -14,4 +15,12 @@ EventSchema.methods.getEventByTitle = function(title, callback){
   Event.findOne(query,callback);
 };
 
+EventSchema.methods.slugify =  function(text) {
+	return text.toString().toLowerCase()
+	.replace(/\s+/g, '-')        // Replace spaces with -
+	.replace(/[^\w\-]+/g, '')   // Remove all non-word chars
+	.replace(/\-\-+/g, '-')      // Replace multiple - with single -
+	.replace(/^-+/, '')          // Trim - from start of text
+	.replace(/-+$/, '');         // Trim - from end of text
+}; 
 module.exports = mongoose.model('Event', EventSchema);
