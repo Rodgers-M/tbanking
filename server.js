6aa228b31cd771fa113dde2e80b8af3b37a92a18
Rodgers-M <mkishagha@gmail.com>
2017-05-25 14:10:32 +0300

    define a middlewre to enable the app serve static files when editing events

--- server.js
+++ server.js
@@ -21,8 +21,8 @@ mongoose.connect(configDB.url);
 //require passport for authentication
 require('./config/passport')(passport);
 
-app.use('/bower_components', express.static(__dirname + '/bower_components'));
 app.use(express.static(__dirname + '/public'));
+app.use('/editevent',express.static(__dirname + '/public'));
 
 app.use(cookieParser());
 app.use(session({
