947837c29d541c6079e62561e673b348fe670857
Rodgers-M <mkishagha@gmail.com>
2017-05-26 19:25:00 +0300

    define a model for  Rsvp

---
+++ app/models/rsvp.js
@@ -0,0 +1,14 @@
+var mongoose   = require('mongoose');
+var Schema     = mongoose.Schema;
+
+var User       =   require('../../app/models/user');
+var Event      =   require('../../app/models/events');
+
+var RsvpSchema =  Schema( {
+   username   : [{type : String, ref:User }],
+   event_slug : [{type : String, ref:Event}]
+ });
+
+
+module.exports = mongoose.model('Rsvp', RsvpSchema);
+
