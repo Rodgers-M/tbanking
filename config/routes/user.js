d52c37fd07316f06f2f8ecdd3458d48f98d9bf6b
Rodgers-M <mkishagha@gmail.com>
2017-06-11 09:52:40 +0300

    add the code to delete and list users

--- config/routes/user.js
+++ config/routes/user.js
@@ -1,8 +1,33 @@
+var passport	  = require('passport');
 var User          = require('../../app/models/user');
 module.exports ={
   new : function(req, res){
     res.render('signup', {
        title   : "Sign Up"
     });
-  }
+  },
+ create : function(req, res, next){
+	passport.authenticate('local-signup',{
+        successRedirect : '/login', //
+        failureRedirect : '/signup', // redirect back to the signup page if there is an error
+        failureFlash : true // allow flash messages
+    })(req, res, next);
+ },
+ index : function(req, res){
+     User.find({}, function(error, users){
+       if(error) res.send(error);
+       res.render('admindashboard/members',{
+           members : users,
+           page   : 'members'
+       });
+     });
+   },
+	delete :function(req, res){
+		User.findOneAndRemove({'local.username' : req.params.member}, function(error, removedMember){
+			if(error) return next(error);
+			req.flash('success', 'Member deleted successfully');
+			res.redirect('/members');
+		});
+	
+	}
 };
